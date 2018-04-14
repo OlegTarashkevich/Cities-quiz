79ec7c49495d30125d0e4f81c6e2d8919ac6c712
Oleg Tarashkevich <oleg.tarashkevich@mobexs.com>
2018-04-14 10:38:14 +0300

    first commit

---
+++ app/src/main/java/com/paralect/citiesquiz/data/DataUtil.kt
@@ -0,0 +1,7 @@
+package com.paralect.citiesquiz.data
+
+/**
+ * Created by Oleg Tarashkevich on 14/04/2018.
+ */
+class DataUtil {
+}
---
+++ app/src/main/java/com/paralect/citiesquiz/data/HighScore.kt
@@ -0,0 +1,8 @@
+package com.paralect.citiesquiz.data
+
+/**
+ * Created by Oleg Tarashkevich on 14/04/2018.
+ */
+class HighScore {
+
+}
