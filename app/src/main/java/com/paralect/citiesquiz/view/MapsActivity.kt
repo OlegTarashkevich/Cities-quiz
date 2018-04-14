b7be0e20619d98631a49045e2e808771a1d2c1e0
Oleg Tarashkevich <oleg.tarashkevich@mobexs.com>
2018-04-14 16:20:14 +0300

    added databinding

--- app/src/main/java/com/paralect/citiesquiz/view/MapsActivity.kt
+++ app/src/main/java/com/paralect/citiesquiz/view/MapsActivity.kt
@@ -1,44 +1,34 @@
 package com.paralect.citiesquiz.view
 
 import android.content.res.Resources
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
 import com.paralect.citiesquiz.R
 import com.paralect.citiesquiz.data.model.GameLevel
 import com.paralect.citiesquiz.data.model.GameResult
+import com.paralect.citiesquiz.databinding.ActivityMapsBinding
 import com.paralect.citiesquiz.presenter.GamePresenter
 import com.paralect.citiesquiz.presenter.IGameView
 import com.paralect.citiesquiz.utils.setVisibile
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, IGameView {
 
-    companion object {
-        val TAG = MapsActivity::class.java.simpleName
-    }
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, IGameView {
 
     private val presenter = GamePresenter()
 
     private lateinit var mMap: GoogleMap
-    private lateinit var mStatusTextView: TextView
-    private lateinit var taskTextView: TextView
-    private lateinit var progressBar: ProgressBar
+    private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-
-        mStatusTextView = findViewById(R.id.status_textview)
-        taskTextView = findViewById(R.id.task_textview)
-        progressBar = findViewById(R.id.progress_bar)
+        binding = DataBindingUtil.setContentView<ActivityMapsBinding>(this, R.layout.activity_maps)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -60,11 +50,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, IGameView {
             onError(e)
         }
 
-        mMap.setOnMapClickListener {
+        binding.placeButton.setOnClickListener {
             if (presenter.isLoaded()) {
+                val center = mMap.cameraPosition.target
                 mMap.clear()
-                mMap.addMarker(MarkerOptions().position(it).title("Your choice"))
-                presenter.setUsersCoordinate(it)
+                presenter.setUsersCoordinate(center)
             }
         }
     }
@@ -72,11 +62,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, IGameView {
 
     // region IGameView
     override fun onLevelLoaded(level: GameLevel) {
-        taskTextView.text = level.details
+        binding.taskTextview.text = level.details
     }
 
     override fun onGameResult(result: GameResult) {
-        taskTextView.text = result.toString()
+        binding.taskTextview.text = result.toString()
     }
 
     override fun onError(e: Throwable) {
@@ -86,7 +76,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, IGameView {
     }
 
     override fun onShowProgress(show: Boolean) {
-        progressBar.setVisibile(show)
+        binding.progressBar.setVisibile(show)
     }
     // endregion
 }
